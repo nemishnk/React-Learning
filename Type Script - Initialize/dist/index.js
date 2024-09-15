6bfce28384589951aff33d3a590c76f9662c9d76
Nemish Karthik <nemish452@gmail.com>
2024-09-15 11:02:11 +0530

    Initial Comit

---
+++ Type Script - Initialize/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function Multiply(a, b) {
+    return a * b;
+}
+const a = 5;
+const b = 5;
+console.log(`${a} * ${b} =${Multiply(a, b)}`);
---
+++ Type Script - Initialize/src/index.ts
@@ -0,0 +1,8 @@
+function Multiply(a:number,b:number):number{
+    return a*b;
+}
+
+const a =5;
+const b= 5;
+
+console.log(`${a} * ${b} =${Multiply(a,b)}`)
